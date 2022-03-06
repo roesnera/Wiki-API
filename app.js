99e3063eedcd8e39d75553f8cbcd4a9a1325f90e
Adam Roesner <roesnera@kenyon.edu>
2022-03-05 21:50:00 -0500

    Adds routing for /articles

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require('lodash');
 
+// initializes express server, sets view engine to ejs so we can use ejs syntax
 const app = express();
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-const yourDB = 'wikiDB'
+// initializes mongodb connection using mongoose driver
+const yourDB = "wikiDB";
 const url = `mongodb://localhost:27017/${yourDB}`;
-mongoose.connect(url);
+mongoose.connect(url, { useNewURLParser: true });
 
 const articleSchema = new mongoose.Schema({
-    title: {
-        type: String
-    },
-    content: {
-        type: String
-    }
+  title: {
+    type: String,
+  },
+  content: {
+    type: String,
+  },
 });
 
-const Article = mongoose.model('Article', articleSchema);
+const Article = mongoose.model("Article", articleSchema);
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, found) {
+      if (!err) {
+        res.send(found);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const title = req.body.title;
+    const content = req.body.content;
 
-app.get('/', function(req, res) {
-    res.send('Hello!');
-})
+    const postArt = new Article({
+      title: title,
+      content: content,
+    });
 
+    postArt.save(function (err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully added article!");
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully deleted collection contents.");
+      }
+    });
+  });
 
-app.listen(3000, function() {
-    console.log('Server lsitening on port 3000')
-})
+app.listen(3000, function () {
+  console.log("Server lsitening on port 3000");
+});
